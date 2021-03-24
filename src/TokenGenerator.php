fe1dd19984b8595d83d2995426992d195e26e441
Barry Coleman <barry@barrycoleman.com>
2021-03-24 10:43:05 -0700

    Added doc for exception

--- src/TokenGenerator.php
+++ src/TokenGenerator.php
@@ -322,6 +322,10 @@ class TokenGenerator {
   }
 }
 
+/**
+ * Class TokenGeneratorException
+ * @package barrycoleman\AkamaiEdgeAuth
+ */
 class TokenGeneratorException extends \Exception {
   public function __construct($message, $code = 0, \Throwable $previous = NULL) {
     parent::__construct($message, $code, $previous);
